19c7bdd5909b77275ab0ba9a7ee5d8a3aeef6b9d
Stephan Kulla <git.mail@kulla.me>
2023-10-22 13:32:02 +0200

    feat: Sequence on the number line

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,74 @@
-import { Bar } from "react-chartjs-2";
+import { Scatter } from "react-chartjs-2";
 import {
-  BarElement,
-  CategoryScale,
   Chart,
   Legend,
+  LineElement,
   LinearScale,
-  Title,
+  PointElement,
   Tooltip,
+  Colors,
 } from "chart.js";
 import "./App.css";
 
-Chart.register(LinearScale, CategoryScale, BarElement, Title, Tooltip, Legend);
+Chart.register(LinearScale, PointElement, LineElement, Tooltip, Legend, Colors);
 
 function App() {
+  const sequence = generateConvergingSequence({
+    target: 2.5,
+    oscillationRange: 2.5,
+    convergenceSpeed: 1.2,
+    sequenceLength: 100,
+  });
+
   const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
+        label: "Sequence",
+        data: sequence.map((x) => ({ x, y: 0 })),
       },
     ],
   };
-  const options = {
-    animation: false,
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  } as const;
 
   return (
     <div className="App">
-      <Bar data={data} options={options}></Bar>
+      <h2>Sequence on the number line</h2>
+      <div style={{ height: 150 }}>
+        <Scatter
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              y: { min: 0, max: 1, display: false },
+              x: { min: 0, max: 5, grid: { display: false } },
+            },
+          }}
+        />
+      </div>
     </div>
   );
+}
+
+function generateConvergingSequence({
+  target,
+  oscillationRange,
+  convergenceSpeed,
+  sequenceLength,
+}: {
+  target: number;
+  oscillationRange: number;
+  convergenceSpeed: number;
+  sequenceLength: number;
+}) {
+  const sequence = [];
+
+  for (let n = 1; n <= sequenceLength; n++) {
+    const randomNumber = 2 * Math.random() - 1;
+    const oscillation =
+      (oscillationRange * randomNumber) / n ** convergenceSpeed;
+    sequence.push(target + oscillation);
+  }
+
+  return sequence;
 }
 
 export default App;
