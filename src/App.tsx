9b10b1f4bee4ac2fd02c3802da9c7b12e210019c
Stephan Kulla <git.mail@kulla.me>
2023-10-22 12:28:30 +0200

    feat: Add first example of a bar chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
+import { Bar } from "react-chartjs-2";
+import {
+  BarElement,
+  CategoryScale,
+  Chart,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
 import "./App.css";
 
+Chart.register(LinearScale, CategoryScale, BarElement, Title, Tooltip, Legend);
+
 function App() {
-  return <div className="App"></div>;
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div className="App">
+      <Bar data={data} options={options}></Bar>
+    </div>
+  );
 }
 
 export default App;
