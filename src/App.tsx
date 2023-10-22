af0f1f29a14667c6706d89ffbef3741a1cc1246b
Stephan Kulla <git.mail@kulla.me>
2023-10-22 15:13:22 +0200

    feat: Add animation

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   PointElement,
   Tooltip,
   Colors,
+  AnimationSpec,
 } from "chart.js";
 import "./App.css";
 
@@ -19,6 +20,43 @@ function App() {
     convergenceSpeed: 1.2,
     sequenceLength: 100,
   });
+
+  const totalDuration = 2000;
+  const delayBetweenPoints = totalDuration / sequence.length;
+  const previousY = (ctx: any) =>
+    ctx.index === 0
+      ? ctx.chart.scales.y.getPixelForValue(100)
+      : ctx.chart
+          .getDatasetMeta(ctx.datasetIndex)
+          .data[ctx.index - 1].getProps(["y"], true).y;
+  const animation = {
+    x: {
+      type: "number",
+      easing: "linear",
+      duration: delayBetweenPoints,
+      from: NaN, // the point is initially skipped
+      delay(ctx: any) {
+        if (ctx.type !== "data" || ctx.xStarted) {
+          return 0;
+        }
+        ctx.xStarted = true;
+        return ctx.index * delayBetweenPoints;
+      },
+    },
+    y: {
+      type: "number",
+      easing: "linear",
+      duration: delayBetweenPoints,
+      from: previousY,
+      delay(ctx: any) {
+        if (ctx.type !== "data" || ctx.yStarted) {
+          return 0;
+        }
+        ctx.yStarted = true;
+        return ctx.index * delayBetweenPoints;
+      },
+    },
+  } as unknown as AnimationSpec<"scatter">;
 
   return (
     <div className="App">
@@ -34,6 +72,7 @@ function App() {
             ],
           }}
           options={{
+            animation,
             maintainAspectRatio: false,
             scales: {
               y: { min: 0, max: 1, display: false },
@@ -53,6 +92,7 @@ function App() {
           ],
         }}
         options={{
+          animation,
           scales: {
             y: { beginAtZero: true },
             x: { min: 0, max: 5 },
